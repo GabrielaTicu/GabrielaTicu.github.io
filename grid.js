502df64c505eeed8feb92d1d61023ab8cfc9c814
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-02-13 17:52:58 +0200

    Add files via upload

--- grid.js
+++ grid.js
@@ -1,3 +1,49 @@
-function change(link, alt) {
+function change(link, alt, id) {
     image.src = link;
+    image.alt = alt;
+}
+
+matterhorn.onmouseover = function(event) {
+    let target = event.target;
+    target.style.background = 'pink';
+}
+matterhorn.onmouseout = function(event) {
+    let target = event.target;
+    target.style.background = '';
+}
+
+denali.onmouseover = function(event) {
+    let target = event.target;
+    target.style.background = 'pink';
+}
+denali.onmouseout = function(event) {
+    let target = event.target;
+    target.style.background = '';
+}
+
+kirkjufell.onmouseover = function(event) {
+    let target = event.target;
+    target.style.background = 'pink';
+}
+kirkjufell.onmouseout = function(event) {
+    let target = event.target;
+    target.style.background = '';
+}
+
+tableMountain.onmouseover = function(event) {
+    let target = event.target;
+    target.style.background = 'pink';
+}
+tableMountain.onmouseout = function(event) {
+    let target = event.target;
+    target.style.background = '';
+}
+
+vinicunca.onmouseover = function(event) {
+    let target = event.target;
+    target.style.background = 'pink';
+}
+vinicunca.onmouseout = function(event) {
+    let target = event.target;
+    target.style.background = '';
 }
